4d1e391b78ce4c96467f6e49d6b5412b344847ca
MariaAlexandraM <maria_moldovan_2003@yahoo.com>
2024-01-08 00:26:04 +0200

    .

---
+++ coding-events/src/main/java/com/example/codingevents/controllers/EventController.java
@@ -0,0 +1,71 @@
+package com.example.codingevents.controllers;
+
+import com.example.codingevents.data.EventCategoryRepository;
+import com.example.codingevents.data.EventRepository;
+import com.example.codingevents.models.Event;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    @Autowired // spring boot o sa dea valori automate, pt ca nu am constructori; = dependency injection
+    private EventRepository eventRepository; // ii ciudat ca ii interfata but oh well... ii ceva ce spring poate face pt ca am pus @Repository si creeaza o clasa in memorie ca sa o implementeze si dupa pe care sa o bage aici, autowired
+
+    // findAll, save, findById - is metode pe care le putem folosi pt ca is implementate in CrudRepository interface, care ii extinsa de EventRepository
+
+    @Autowired
+    private EventCategoryRepository eventCategoryRepository;
+
+    @GetMapping
+    public String displayAllEvents(Model model) {
+        model.addAttribute("title", "All Events");
+        // model.addAttribute("events", EventData.getAllEvents()); // inainte de crearea repository ului
+         model.addAttribute("events", eventRepository.findAll());
+        return "events/index";
+    }
+
+    @GetMapping("create")
+    public String displayCreateEventForm(Model model) {
+        model.addAttribute("title", "Create event");
+        model.addAttribute(new Event());
+        model.addAttribute("categories", eventCategoryRepository.findAll());
+        return "events/create";
+    }
+
+    @PostMapping("create")
+    public String processCreateEventForm(@ModelAttribute @Valid Event newEvent,
+                                         Errors errors, Model model) {
+        if(errors.hasErrors()) {
+            model.addAttribute("title", "Create Event");
+            return "events/create";
+        }
+        //EventData.add(newEvent);
+        eventRepository.save(newEvent);
+        return "redirect:/events";
+    }
+
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model) {
+        model.addAttribute("title", "Delete Event");
+        model.addAttribute("events", eventRepository.findAll());
+        return "events/delete";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteEventForm(@RequestParam(required = false) int[] eventIds) {
+
+        if(eventIds != null) {
+            for (int id : eventIds) {
+                eventRepository.deleteById(id);
+            }
+        }
+        return "redirect:/events";
+    }
+
+}
---
+++ coding-events/src/main/java/com/example/codingevents/models/AbstractEntity.java
@@ -0,0 +1,33 @@
+package com.example.codingevents.models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+import java.util.Objects;
+
+@MappedSuperclass // the fields in this class should b pushed down in the tables for the objects that extend it;
+// aici am campu de id, da cand se creeaza un obiect Event sau EventCategory, o sa se foloseasca de id-u asta chiar daca ii privat aici
+public abstract class AbstractEntity {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity entity = (AbstractEntity) o;
+        return id == entity.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
