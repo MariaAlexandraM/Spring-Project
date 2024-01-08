ecc57e143d79355a9409559a1c08dba452cc4425
MariaAlexandraM <maria_moldovan_2003@yahoo.com>
2024-01-08 23:51:29 +0200

    nu mai pot

---
+++ coding-events/src/main/java/com/example/codingevents/data/EventRepository.java
@@ -0,0 +1,12 @@
+package com.example.codingevents.data;
+
+import com.example.codingevents.models.Event;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends CrudRepository<Event, Integer> { // tipu de obiect si primary key type
+
+
+
+}
